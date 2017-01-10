a619bf2b95653151228002b1ed083a6a704f75c1
Albert van Dalen <albertfora@avdweb.nl>
2017-01-10 18:02:09 +0100

    Add files via upload

---
+++ Eeprom.ino
@@ -0,0 +1,30 @@
+const unsigned maxWrites = 100; 
+const unsigned initValue = 123456; // take another value for new initialization
+
+void Eeprom::init()
+{ setMemPool(0, EEPROMSizeATmega328);
+  setMaxAllowedWrites(maxWrites); 
+  storedInitValue = getAddress(sizeof(unsigned));
+  preweld_ms = getAddress(sizeof(unsigned));
+  pause_ms = getAddress(sizeof(unsigned));
+  weld_ms = getAddress(sizeof(unsigned)); 
+  if(!initialized()) write(); // fill an empty EEPROM
+}
+
+void Eeprom::read()
+{ menuItems[0].upDownVal.value = readInt(preweld_ms);
+  menuItems[1].upDownVal.value = readInt(pause_ms);
+  menuItems[2].upDownVal.value = readInt(weld_ms);
+}
+
+void Eeprom::write()
+{ writeInt(storedInitValue, initValue);
+  writeInt(preweld_ms, menuItems[0].upDownVal.value);
+  writeInt(pause_ms, menuItems[1].upDownVal.value);
+  writeInt(weld_ms, menuItems[2].upDownVal.value);
+}
+
+bool Eeprom::initialized()
+{ return readInt(storedInitValue) == initValue;
+}
+
